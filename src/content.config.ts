893829d0f32d1ce727c0d9c93135b0d5e3e57ca0
0xleodevv <github.projector740@simplelogin.com>
2026-05-18 18:23:11 +0200

    ref: simplify content components

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,15 +2,27 @@ import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 
+const baseContentSchema = z.object({
+  title: z.string(),
+  date: z.coerce.date(),
+});
+
 const blog = defineCollection({
   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-  schema: z.object({
-    title: z.string(),
+  schema: baseContentSchema.extend({
     description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
     readTime: z.string(),
   }),
 });
 
-export const collections = { blog };
+const work = defineCollection({
+  loader: glob({ base: "./src/content/work", pattern: "**/*.{md,mdx}" }),
+  schema: baseContentSchema.extend({}),
+});
+
+const projects = defineCollection({
+  loader: glob({ base: "./src/content/projects", pattern: "**/*.{md,mdx}" }),
+  schema: baseContentSchema.extend({}),
+});
+
+export const collections = { blog, work, projects };
