d4afa708ee6204e54c2aea8b42c695e9f283c138
0xleodevv <github.projector740@simplelogin.com>
2026-05-21 10:13:17 +0200

    feat: make portfolio schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,6 +8,13 @@ const baseContentSchema = z.object({
   date: z.coerce.date(),
 });
 
+const portfolioItemSchema = baseContentSchema.extend({
+  organization: z.string(),
+  endDate: z.union([z.coerce.date(), z.literal("present")]),
+  technology: z.array(string()),
+  link: z.url(),
+});
+
 const blog = defineCollection({
   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
   schema: baseContentSchema.extend({
@@ -18,24 +25,12 @@ const blog = defineCollection({
 
 const work = defineCollection({
   loader: glob({ base: "./src/content/work", pattern: "**/*.{md,mdx}" }),
-  schema: baseContentSchema.extend({
-    description: z.string(),
-    organization: z.string(),
-    endDate: z.coerce.date(),
-    technology: z.array(string()),
-    link: z.url(),
-  }),
+  schema: portfolioItemSchema,
 });
 
 const projects = defineCollection({
   loader: glob({ base: "./src/content/projects", pattern: "**/*.{md,mdx}" }),
-  schema: baseContentSchema.extend({
-    description: z.string(),
-    organization: z.string(),
-    endDate: z.coerce.date(),
-    technology: z.array(string()),
-    link: z.url(),
-  }),
+  schema: portfolioItemSchema,
 });
 
 export const collections = { blog, work, projects };
